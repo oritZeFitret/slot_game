83597d5a436416eb5e2d9c2e6b73fac545718995
Bisrat <bisrattegegn8@gmail.com>
2022-06-10 18:05:20 +0300

    Uses provided assets

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: document.body.clientWidth,
     height: document.body.clientHeight,
+    background: 0x060403,
     physics: {
         default: 'arcade',
         arcade: {
@@ -15,32 +16,30 @@ const config = {
     }
 };
 
-const game = new    Phaser.Game(config);
-const spriteScale = 0.25;
+const game = new Phaser.Game(config);
+const spriteScale = .7;
 const spriteSize = 256;
 const speed = 1200;
 
-let cheatChosen = "";
+let cheatChosen = [];
 
 let firstColumn = [];
 let secondColumn = [];
 let thirdColumn = [];
 let rollHasStarted = false;
-let collisionBlockSprite;
-let startButton;
-let cheatBanana;
-let cheatCherry;
-let cheatBlackBerry;
 
 function preload(){
     this.load.image("banana", "./assets/banana.png");
     this.load.image("cherry", "./assets/cherry.png");
     this.load.image("blackberry", "./assets/blackberry.png");
-    this.load.image("reelBackground", "./assets/reel_bk.png");
+    this.load.image("spin", "./assets/spin.png");
+    this.load.image("spinGray", "./assets/spinGray.png");
+    this.load.image("win", "./assets/win.png");
+    this.load.image("reelBackground", "./assets/reelBackground.png");
+    this.load.image("reelForeground", "./assets/reelForeground.png");
 }
 
 function create(){
-
     startNewRoll(this);
     addCheatButtons(this);
 }
@@ -51,10 +50,10 @@ function update(){
     for(let j = 0; j < 3; j++){
         for(let i = 0; i < this.fruits[j].length; i++){
             if(this.fruits[j][i]?.body?.position?.y >= centerY + spriteScale * spriteSize ||
-                this.fruits[j][i]?.body?.velocity?.y <= 0){
-                    this.fruits[j][i].body.setVelocity(0);
-                    this.fruits[j][i].body.setAcceleration(0);
-                    this.fruits[j][i].body.allowGravity = false;
+            this.fruits[j][i]?.body?.velocity?.y <= 0){
+                this.fruits[j][i].body.setVelocity(0);
+                this.fruits[j][i].body.setAcceleration(0);
+                this.fruits[j][i].body.allowGravity = false;
             }
         }
     }
@@ -65,78 +64,21 @@ function addCheatButtons(_this){
 
     const centerY = _this.cameras.main.centerY;
 
-    cheatBanana = _this.add.text(20, centerY - 100, "banana");
-    cheatBanana.setStyle({fill: "yellow"});
-    cheatBanana
-    .setInteractive({ useHandCursor: true })
-    .on('pointerdown', () => {
-        if(cheatChosen === "banana"){
-            cheatBanana.text = "banana";
-            cheatChosen = "";
-        } 
-        else{
-            cheatBanana.text = "✓ banana";
-            cheatCherry.text = "cherry";
-            cheatBlackBerry.text = "blackberry";
-            cheatChosen = "banana";
-        } 
-    });
     
-    cheatCherry = _this.add.text(20, centerY - 80, "cherry");
-    cheatCherry.setStyle({fill: "red"});
-    cheatCherry
-    .setInteractive({ useHandCursor: true })
-    .on('pointerdown', () => {
-        if(cheatChosen === "cherry"){
-            cheatCherry.text = "cherry";
-            cheatChosen = "";
-        } 
-        else {
-            cheatCherry.text = "✓ cherry";
-            cheatBanana.text = "banana";
-            cheatBlackBerry.text = "blackberry";
-            cheatChosen = "cherry";
-        }
-    });
-
-    cheatBlackBerry = _this.add.text(20, centerY - 60, "blackberry");
-    cheatBlackBerry.setStyle({fill: "gray"});
-    cheatBlackBerry
-    .setInteractive({ useHandCursor: true })
-    .on('pointerdown', () => {
-        if(cheatChosen === "blackberry"){
-            cheatBlackBerry.text = "blackberry";
-            cheatChosen = "";
-        } 
-        else {
-            cheatCherry.text = "cherry";
-            cheatBanana.text = "banana";
-            cheatBlackBerry.text = "✓ blackberry";
-            cheatChosen = "blackberry"
-        };
-    });
 }
 
 function removeCheatButtons(){
-    cheatChosen = "";
-    cheatBanana?.destroy?.();
-    cheatCherry?.destroy?.();
-    cheatBlackBerry?.destroy?.();
+    
 }
 
 function addSpinButton(_this){
     const centerX = _this.cameras.main.centerX;
     const centerY = _this.cameras.main.centerY;
 
-    startButton = _this.add.text(centerX, centerY + spriteSize * 3 * spriteScale, 'Spin', {});
-    startButton
-    .setOrigin(0.5)
-    .setPadding(10)
-    .setStyle({ backgroundColor: '#888' })
+    _this.startButton = _this.add.image(centerX, centerY + spriteSize * 1.2 * spriteScale, "spinGray")
+    .setScale(0.6)
     .setInteractive({ useHandCursor: true })
     .on('pointerdown', () => startNewRoll(_this))
-    .on('pointerover', () => startButton.setStyle({ fill: '#f39c12' }))
-    .on('pointerout', () => startButton.setStyle({ fill: '#FFF' }));
 }
 
 function startNewRoll(_this){
@@ -144,10 +86,10 @@ function startNewRoll(_this){
         const centerX = _this.cameras.main.centerX;
         const centerY = _this.cameras.main.centerY;
 
-        _this.bigWinText?.destroy?.();
+        _this.bigWin?.destroy?.();
         
         rollHasStarted = true;
-        startButton?.setStyle({ fill: "#888"});
+        _this.startButton?.setTexture("spinGray");
 
         destroyElements(_this);
         
@@ -155,19 +97,28 @@ function startNewRoll(_this){
         drawReelBackground(_this);
         addFruits(_this);
         drawMasks(_this);
+        drawReelForeground(_this);
         addSpinButton(_this);
 
-        removeCheatButtons();
         addCheatButtons(_this);
 
         setTimeout(function(){
-            startButton?.setStyle({ backgroundColor: "#111"});
+            _this.startButton?.setTexture("spin");
             rollHasStarted = false;
             if(firstColumn[0] === secondColumn[0] && secondColumn[0] === thirdColumn[0]){
-                _this.bigWinText = _this.add.text(centerX - spriteScale * spriteSize * 0.5, centerY - spriteScale * spriteSize * 1.2, "Big Win", { fill: "gold", fontSize: 24, fontWeight: 400 });
+                _this.bigWin = _this.add.image(centerX, centerY - spriteScale * spriteSize * 1.2, "win")
+                .setScale(spriteScale);
             }
         }, 3500);
     }
+}
+
+function drawReelForeground(_this){
+    
+    const centerX = _this.cameras.main.centerX;
+    const centerY = _this.cameras.main.centerY;
+    const reelFg = _this.add.image(centerX, centerY, "reelForeground");
+    reelFg.setScale(spriteScale * 0.8);
 }
 
 function drawReelBackground(_this){
@@ -175,7 +126,7 @@ function drawReelBackground(_this){
     const centerX = _this.cameras.main.centerX;
     const centerY = _this.cameras.main.centerY;
     const reelBk = _this.add.image(centerX, centerY, "reelBackground");
-    reelBk.setScale(spriteScale);
+    reelBk.setScale(spriteScale * 0.8);
 }
 
 function addFruits(_this){
@@ -187,20 +138,21 @@ function addFruits(_this){
     const centerY = _this.cameras.main.centerY;
 
     const distance = Math.pow(speed, 2) / 800;
+    const offset = 50 * spriteScale;
 
     for(let i = 0; i < firstColumn.length; i++){
-        _this.fruits[0][i] = _this.physics.add.sprite(centerX - (37.5 + spriteSize) * spriteScale, centerY - distance + 15 + spriteSize * spriteScale * i, firstColumn[i]);
+        _this.fruits[0][i] = _this.physics.add.sprite(centerX - (37.5 + spriteSize) * spriteScale, centerY - distance + offset + spriteSize * spriteScale * i, firstColumn[i]);
         _this.fruits[0][i].setScale(spriteScale);
         _this.fruits[0][i].body.setVelocity(0, speed);
 
     }
     for(let i = 0; i < secondColumn.length; i++){
-        _this.fruits[1][i] = _this.physics.add.sprite(centerX,centerY - distance * 1.44 + 15 + spriteSize * spriteScale * i, secondColumn[i]);
+        _this.fruits[1][i] = _this.physics.add.sprite(centerX,centerY - distance * 1.44 + offset + spriteSize * spriteScale * i, secondColumn[i]);
         _this.fruits[1][i].setScale(spriteScale);
         _this.fruits[1][i].body.setVelocity(0, speed * 1.2);
     }
     for(let i = 0; i < thirdColumn.length; i++){
-        _this.fruits[2][i] = _this.physics.add.sprite(centerX + (37.5 + spriteSize) * spriteScale, centerY - distance * 1.96 + 15 + spriteSize * spriteScale * i, thirdColumn[i]);
+        _this.fruits[2][i] = _this.physics.add.sprite(centerX + (37.5 + spriteSize) * spriteScale, centerY - distance * 1.96 + offset + spriteSize * spriteScale * i, thirdColumn[i]);
         _this.fruits[2][i].setScale(spriteScale);
         _this.fruits[2][i].body.setVelocity(0, speed * 1.4);
     }
@@ -211,9 +163,9 @@ function drawMasks(_this){
     const centerY = _this.cameras.main.centerY;
     const width = document.body.clientWidth;
     const height = document.body.clientHeight;
-    const maskHeight = (height - spriteScale * spriteSize) / 2;
-    _this.add.rectangle(centerX, maskHeight / 2, width, maskHeight, 0x000000, 1);
-    _this.add.rectangle(centerX, centerY + (spriteSize * spriteScale + maskHeight) * .5, width, maskHeight, 0x000000, 1);
+    const maskHeight = (height - 1.75 * spriteScale * spriteSize) / 2;
+    _this.add.rectangle(centerX, maskHeight / 2, width, maskHeight, 0x060403, 1);
+    _this.add.rectangle(centerX, centerY + (1.75 * spriteSize * spriteScale + maskHeight) * .5, width, maskHeight, 0x060403, 1);
 }
 
 function destroyElements(_this){
